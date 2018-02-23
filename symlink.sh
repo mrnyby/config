fad74be25f4c73f6adc212fdef5bd6f8bf40e5b3
Aaron Russell <russell.aaron94@gmail.com>
2018-02-22 18:24:42 -0700

    Added script that applies some configuration in this repository through symlinks

---
+++ symlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Creates symlinks in appropriate locations to files in this repository
+
+ln -sf i3 ~/.config/i3/config
+ln -sf i3status ~/.config/i3status/config
+ln -sf vimrc ~/.vimrc
