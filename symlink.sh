a1f2215c3341d7a9893345ca354a34d1a41bd45c
Aaron Russell <arussell@clearwateranalytics.com>
2018-03-02 10:41:28 -0700

    Removing symlink script since it's broken

--- symlink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Creates symlinks in appropriate locations to files in this repository
-
-ln -sf i3 ~/.config/i3/config
-ln -sf i3status ~/.config/i3status/config
-ln -sf vimrc ~/.vimrc
