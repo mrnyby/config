b88d1b811c52de12f18816df5ceb3c921de1c4d2
Aaron Russell <russell.aaron94@gmail.com>
2020-02-28 18:18:24 -0700

    Remove VSCode config

--- setup.sh
+++ setup.sh
@@ -2,15 +2,8 @@
 
 # This setup script creates symlinks throughout the system that point to this repository's configuration files.
 
-pwd=`pwd`
+PWD=`pwd`
 
 # Vim
 rm -rf ~/.vimrc
-ln -s $pwd/vimrc ~/.vimrc
-
-# VSCode
-if [ -f ~/.config/Code/User/settings.json ]
-then
-    rm -rf ~/.config/Code/User/settings.json
-fi
-ln -s $pwd/settings.json ~/.config/Code/User/settings.json
+ln -s $PWD/vimrc ~/.vimrc
