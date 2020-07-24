c190b952fcd35256da5bfd471e7e98e2662331db
Aaron Russell <russell.aaron94@gmail.com>
2020-07-24 13:09:30 -0700

    Remove VSCode setup

--- setup.sh
+++ setup.sh
@@ -7,10 +7,3 @@ PWD=`pwd`
 # Vim
 rm -rf ~/.vimrc
 ln -s $PWD/vimrc ~/.vimrc
-
-# VSCode
-if [ -f ~/.config/Code/User/settings.json ]
-then
-	rm -rf ~/.config/Code/User/settings.json
-fi
-ln -s $PWD/settings.json ~/.config/Code/User/settings.json
