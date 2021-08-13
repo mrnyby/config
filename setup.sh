5f2ab5190a92061e90e88ddc009dd83767c7b989
Aaron Russell <russell.aaron94@gmail.com>
2021-08-13 10:43:37 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ PWD=`pwd`
 # Vim
 rm -rf ~/.vimrc
 ln -s $PWD/vimrc ~/.vimrc
+
+echo "Additional setup is required!
+  1. Copy this repository's settings.json file to wherever Visual Studio Code stores that kind of thing on your system.
