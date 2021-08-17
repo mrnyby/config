199031d80840cb25e4fdd9221ddf76e0ce52b3a1
Aaron Russell <aaron@localhost.localdomain>
2021-08-16 23:37:34 -0600

    Fix missing quotes

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ rm -rf ~/.vimrc
 ln -s $PWD/vimrc ~/.vimrc
 
 echo "Additional setup is required!
-  1. Copy this repository's settings.json file to wherever Visual Studio Code stores that kind of thing on your system.
+  1. Copy this repository's settings.json file to wherever Visual Studio Code stores that kind of thing on your system."
